8d28f226f9487129cad5ad29583767978d69fb5b
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-03-13 11:24:23 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,19 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    #アクション内で、保存できた場合とできなかった場合で条件分岐のため、newアクションから記述している
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
 end
